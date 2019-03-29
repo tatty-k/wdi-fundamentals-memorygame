9a80ec0945d0e5a5e2da97951063e15b6bb9abb1
Tatty <tattyklass@gmail.com>
2019-03-28 17:14:29 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("user flipped " + cardTwo);
-console.log("user flipped " + cardThree);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log(cardsInPlay);
+
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else{alert("Sorry, try again.");}
+}
+
+
+
+
+
+
