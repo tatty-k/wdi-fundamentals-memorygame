96c1ea4b0ae35328e2a79708b02b1d811fe0988c
Tatty <tattyklass@gmail.com>
2019-04-02 15:03:20 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,20 +27,29 @@ var checkForMatch = function(){
         if(cardsInPlay[0] === cardsInPlay[1]){
             alert("You found a match!");}
         else{alert("Sorry, try again.");}
-    }
+    };
 };
 
-var flipCard = function(cardId){
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].suit);
-    console.log(cards[cardId].cardImage)
+var flipCard = function(){
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
+    
     
     cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+    for (var i=0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    };
+};
+
+createBoard()
 
 
 
